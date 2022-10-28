7cd5d3411305443eaa61b0380d4f564921eb03ba
Diss <nikushkin@mail.ru>
2022-10-28 20:37:24 +0500

    Generate Thumbnail By Product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -15,7 +15,12 @@ class ProductFactory extends Factory
     {
         return [
             'title' => ucfirst($this->faker->words(2, true)),
-            'thumbnail' => '',
+            // TODO HOME Custom Provider Faker
+            'thumbnail' => $this->faker->file(
+                base_path('/tests/Fixtures/images/products'),
+                storage_path('/app/public/images/products'),
+                false
+            ),
             'price' => $this->faker->numberBetween(1000, 100000),
             'brand_id' => Brand::query()->first()->inRandomOrder()->value('id'),
         ];
