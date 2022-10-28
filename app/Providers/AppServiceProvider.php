8d661d51aa89e6f61d839062e799d85fde3e35ef
Diss <nikushkin@mail.ru>
2022-10-28 14:07:24 +0500

    Fix AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,36 +11,20 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
-
     public function boot(): void
     {
         Model::shouldBeStrict(app()->isLocal());
 
         if (app()->isProduction()) {
-            DB::whenQueryingForLongerThan(
-                CarbonInterval::seconds(5),
-                function (Connection $connection) {
-                    logger()
-                        ->channel('telegram')
-                        ->debug('whenQueryingForLongerThan: ' . $connection->totalQueryDuration());
-                }
-            );
-
             DB:listen(function ($query) {
                 if ($query->time > 100) {
                     logger()
                         ->channel('telegram')
-                        ->debug('whenQueryingForLongerThan: ' . $query->sql, $query->bindings);
+                        ->debug('query longer than 1ms: ' . $query->sql, $query->bindings);
                 }
             });
 
-            $kernel = app(Kernel::class);
-
-            $kernel->whenRequestLifecycleIsLongerThan(
+            app(Kernel::class)->whenRequestLifecycleIsLongerThan(
                 CarbonInterval::seconds(4),
                 function () {
                     logger()
