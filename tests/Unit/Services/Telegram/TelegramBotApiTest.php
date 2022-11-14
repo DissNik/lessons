2aa4368c17d63534350b58ccac0bb780712684da
Diss <nikushkin@mail.ru>
2022-11-14 17:39:28 +0500

    Interface TelegramBotApi & Tests

--- tests/Unit/Services/Telegram/TelegramBotApiTest.php
+++ tests/Unit/Services/Telegram/TelegramBotApiTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services\Telegram;
 
 use Illuminate\Support\Facades\Http;
 use Services\Telegram\TelegramBotApi;
+use Services\Telegram\TelegramBotApiContract;
 use Tests\TestCase;
 
 class TelegramBotApiTest extends TestCase
@@ -17,5 +18,25 @@ class TelegramBotApiTest extends TestCase
         $result = TelegramBotApi::sendMessage('', 1, 'Testing');
 
         $this->assertTrue($result);
+    }
+
+    public function test_send_message_success_by_fake_instance(): void
+    {
+        TelegramBotApi::fake()
+            ->returnTrue();
+
+        $result = app(TelegramBotApiContract::class)::sendMessage('', 1, 'Testing');
+
+        $this->assertTrue($result);
+    }
+
+    public function test_send_message_fail_by_fake_instance(): void
+    {
+        TelegramBotApi::fake()
+            ->returnFalse();
+
+        $result = app(TelegramBotApiContract::class)::sendMessage('', 1, 'Testing');
+
+        $this->assertFalse($result);
     }
 }
